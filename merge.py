fda57432b33f9583aa1479c71da0509c449480a6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-11 14:30:10 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -16,6 +16,10 @@ def parseICalFile(theFilename):
 			iCalData = {}
 		elif iCalState == ICALINVEVENT and iCalLine.startswith("DTSTART:"):
 			iCalData["StartDate"] = iCalLine.split(":")[1]
+		elif iCalState == ICALINVEVENT and iCalLine.startswith("DTEND:"):
+			iCalData["EndDate"] = iCalLine.split(":")[1]
+		elif iCalState == ICALINVEVENT and iCalLine.startswith("DESCRIPTION:"):
+			iCalData["Description"] = iCalLine.split(":")[1]
 		elif iCalState == ICALINVEVENT and iCalLine.startswith("END:VEVENT"):
 			iCalState = ICALSTART
 			print(iCalData)
