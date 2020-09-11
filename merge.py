fbe10b07e88a12114b880f29f066c860a563f33d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-11 16:20:11 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -62,6 +62,9 @@ if len(sys.argv) == 1:
 if sys.argv[1] == "--week-to-view":
 	if len(sys.argv) == 6:
 		startDate = datetime.datetime.strptime(sys.argv[2], "%Y%m%d")
+		if not startDate.weekday == 0:
+			print("ERROR: Start date is not a Monday.")
+			sys.exit(0)
 		noOfWeeks = int(sys.argv[3])
 		parseICalFile(sys.argv[4])
 		with zipfile.ZipFile(sys.argv[5], "r") as templateDocx:
