65dfc5352bbf7558b0518f767896e8ce63be75a6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-11 13:19:47 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -2,4 +2,13 @@
 import sys
 
 if len(sys.argv) == 1:
-	print("DOCX Merge!")
+	print("DOCX Merge - merges data into DOCX templates. Usage:")
+	print("merge.py --week-to-view data.ical template.docx")
+	sys.exit(0)
+	
+if sys.argv[1] == "--week-to-view":
+	if len(sys.argv) == 4:
+		print(sys.argv[2])
+		print(sys.argv[3])
+	else:
+		print("ERROR: week-to-view - incorrect number of parameters.")
