6cdc1bbe58cff0d375f75f0314d66a87cc123ea1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-11 14:11:38 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 import sys
 
+def parseICalFile(theFilename)
+	iCalHandle = open(theFilename)
+	for iCalLine in iCalHandle.readlines():
+		print(iCalLine)
+	iCalHandle.close()
+
 if len(sys.argv) == 1:
 	print("DOCX Merge - merges data into DOCX templates. Usage:")
 	print("merge.py --week-to-view data.ical template.docx")
 	sys.exit(0)
 	
 if sys.argv[1] == "--week-to-view":
-	print(len(sys.argv))
 	if len(sys.argv) == 4:
-		print(sys.argv[2])
-		print(sys.argv[3])
+		parseICal(sys.argv[2])
+		#print(sys.argv[3])
 	else:
 		print("ERROR: week-to-view - incorrect number of parameters.")
