06d027745d25138a55bf1388b6ff8f8b18c7279e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-13 23:10:46 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -99,6 +99,8 @@ if sys.argv[1] == "--week-to-view":
 		templateDocx = zipfile.ZipFile(sys.argv[6], "w")
 		for root, dirs, files in os.walk("templateTemp/"):
 			for file in files:
+				print("File: " + os.path.join(root, file))
+				print("Arc: " + file)
 				templateDocx.write(os.path.join(root, file))
 		templateDocx.close()
 	else:
