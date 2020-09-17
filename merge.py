ba77d4fe33780cfe6025bc0041195f2963db6b65
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-17 16:52:20 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -43,8 +43,10 @@ def addCalendarItem(theYear, theMonth, theDay, theItem):
 	calendar[theYear][theMonth][theDay].append(theItem)
 	
 def normaliseString(theString):
-	return(theString.replace("\\n","\n").replace("\\,",",").replace(" · ","").strip())
-	#return(theString.replace("\\n","\n").replace("\\,",",").strip())
+	#return(theString.replace("\\n","\n").replace("\\,",",").replace(" · ","").strip())
+	result = theString.replace("\\n","\n").replace("\\,",",").strip()
+	print("AAA" + result + "BBB")
+	return(result)
 
 def parseICalFile(theFilename):
 	iCalState = ICALSTART
