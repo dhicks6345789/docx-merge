e0de401751ec6eb1a884daafaa5bc1db9f616739
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-09-17 11:30:35 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -91,8 +91,7 @@ if sys.argv[1] == "--week-to-view":
 				if today.year in calendar.keys():
 					if today.month in calendar[today.year].keys():
 						if today.day in calendar[today.year][today.month].keys():
-							#weekToViewText = weekToViewText.replace(dayString, str(calendar[today.year][today.month][today.day]))
-							weekToViewText = weekToViewText.replace(dayString, calendar[today.year][today.month][today.day])
+							weekToViewText = weekToViewText.replace(dayString, str(calendar[today.year][today.month][today.day]))
 			newDocxText = newDocxText + weekToViewText
 		newDocxText = newDocxText + docxText[bodyEnd:]
 		textHandle = open(TEMPLATETEMP + "word/document.xml", "w")
